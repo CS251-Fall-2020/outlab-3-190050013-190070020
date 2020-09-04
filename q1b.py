023daffcd9c10f44087cbd33922cbeeb81a9bf87
Aman <amanyadav@cse.iitb.ac.in>
2020-09-05 01:09:29 +0530

    q1b.py modified to handle multiple pattern matching in the message and a lot other corner cases

--- q1b.py
+++ q1b.py
@@ -8,22 +8,56 @@ def main():
 	args = parser.parse_args()
 	fin = args.m
 	n = int(fin.readline())
+	found_at_least_one_matching_pattern = False
 	for line in fin:
-		m = re.search(r"\$\(.+\#.+\)\$", line)
-		if(m):
-			combination = line[m.start()+2:m.end()-2]
-			combinationlist = combination.split(')#(')
-			for i in range(0, len(combinationlist)):
-				combinationlist[i] = combinationlist[i].split(',')
-			for single_list in combinationlist:
+		pattern = re.compile(r"\$\(.+\)\#\(.+\)\$")
+		r = pattern.search(line)
+		if(r==None):
+			continue
+		starts = []
+		end = 0
+		while r:
+			starts.append(r.start())
+			end = r.end()
+			r = pattern.search(line,r.start() + 2)
+		starts.append(end)
+		matches =[]
+		for i in range(0, len(starts)-1):
+			m = pattern.search(line, starts[i], starts[i+1])
+			if(m):
+				matches.append(m)
+		for i in range(0, len(matches)):
+			m = matches[i]
+			m_temp = m
+			while(m):
+				m_temp = m
+				m = pattern.search(line, m.start(), m.end()-2)
+			matches[i] = m_temp
+
+
+		for match in matches:
+			# print(match)
+			combinations = match.group()[2:-2]
+			# print(combinations)
+			combination_list = combinations.split(')#(')
+			# print(combination_list)
+			for single_list in combination_list:
+				single_list_split = single_list.split(',')
+				try:
+					for i in range(0, len(single_list_split)):
+						single_list_split[i] = int(single_list_split[i])
+				except:
+						continue
+				# print(single_list_split)
+				found_at_least_one_matching_pattern = True
 				sum = RingInt(0, n)
-				for i in range(1, len(single_list)+1):
-					sum += RingInt(i, n)*RingInt(int(single_list[i-1]), n)
+				for i in range(1, len(single_list_split)+1):
+					sum += RingInt(i, n)*RingInt(single_list_split[i-1], n)
 				if sum!=RingInt(0, n):
 					print("CORRUPTED")
 					return 0
-			print("OK")
-			return 0
+	if(found_at_least_one_matching_pattern==True):
+		print("OK")
 	fin.close()
 
 if __name__=="__main__":
